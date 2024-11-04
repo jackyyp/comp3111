5ca4caaaa127a22fc1b5b4a9645196da60246f47
Shao-Fu-Wang <michaelwang920324@gmail.com>
2024-11-05 00:05:07 +0800

    Change database to sqllite

--- src/main/java/comp3111/examsystem/database/DatabaseConnection.java
+++ src/main/java/comp3111/examsystem/database/DatabaseConnection.java
@@ -5,11 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/examsystem";
-    private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String URL = "jdbc:sqlite:examsystem.db";
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        return DriverManager.getConnection(URL);
     }
 }
