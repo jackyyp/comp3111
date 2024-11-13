5ac4ef407a759bd35ce80958320d43b022a9e0d7
jackyyp <chpoonal@connect.ust.hk>
2024-11-13 23:09:46 +0800

    3.2: Handling manager login. added manager table, implemented ManagerLoginController

--- src/main/java/comp3111/examsystem/controller/ManagerMainController.java
+++ src/main/java/comp3111/examsystem/controller/ManagerMainController.java
@@ -1,12 +1,15 @@
 package comp3111.examsystem.controller;
 
 import comp3111.examsystem.Main;
+import comp3111.examsystem.model.ManagerControllerModel;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import lombok.Setter;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,6 +17,10 @@ import java.util.ResourceBundle;
 public class ManagerMainController implements Initializable {
     @FXML
     private VBox mainbox;
+    @FXML
+    private Label errorLabel;
+    @Setter
+    private ManagerControllerModel dataModel;
 
     public void initialize(URL location, ResourceBundle resources) {
     }
---
+++ src/main/java/comp3111/examsystem/model/ManagerControllerModel.java
@@ -0,0 +1,8 @@
+package comp3111.examsystem.model;
+
+import lombok.Data;
+
+@Data
+public class ManagerControllerModel {
+    private String username;
+}
