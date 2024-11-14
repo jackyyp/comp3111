1d7d80b9e8d8dce5cf8c3e98c5dee5298f586855
jackyyp <chpoonal@connect.ust.hk>
2024-11-14 23:51:37 +0800

    3.3.2 & 3.3.3: Handling the Teacher&Course Management by Manager, implementation with same logic with 3.3.1

--- src/main/java/comp3111/examsystem/controller/ManagerMainController.java
+++ src/main/java/comp3111/examsystem/controller/ManagerMainController.java
@@ -1,17 +1,14 @@
 package comp3111.examsystem.controller;
 
 import comp3111.examsystem.Main;
-import comp3111.examsystem.model.ManagerControllerModel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import lombok.Setter;
+
 
 import java.io.IOException;
 import java.net.URL;
@@ -19,13 +16,6 @@ import java.util.ResourceBundle;
 
 
 public class ManagerMainController implements Initializable {
-    @FXML
-    private VBox mainbox;
-    @FXML
-    private Label errorLabel;
-    @Setter
-    private ManagerControllerModel dataModel;
-
     public void initialize(URL location, ResourceBundle resources) {
     }
 
@@ -45,11 +35,33 @@ public class ManagerMainController implements Initializable {
     }
 
     @FXML
-    public void openTeacherManageUI() {
+    public void openTeacherManageUI(ActionEvent event) {
+        closeCurrentStage(event);
+        try {
+            Stage stage = new Stage();
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("TeacherManagementUI.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            stage.setTitle("Teacher Management");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
-    public void openCourseManageUI() {
+    public void openCourseManageUI(ActionEvent event) {
+        closeCurrentStage(event);
+        try {
+            Stage stage = new Stage();
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("CourseManagementUI.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            stage.setTitle("Course Management");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
