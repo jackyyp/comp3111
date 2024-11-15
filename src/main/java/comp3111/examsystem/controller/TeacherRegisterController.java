a3af194c09870373e40861caaeb75da270c65889
wcy <wongcheukyuen2002@gmail.com>
2024-11-15 19:35:23 +0800

    added position into teacher registration interface allowing flexible updating of exams content

--- src/main/java/comp3111/examsystem/controller/TeacherRegisterController.java
+++ src/main/java/comp3111/examsystem/controller/TeacherRegisterController.java
@@ -13,6 +13,7 @@ import javafx.util.Duration;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
@@ -23,6 +24,8 @@ public class TeacherRegisterController implements Initializable {
     private TextField nameTxt;
     @FXML
     private ComboBox<String> genderComboBox;
+    @FXML
+    private ComboBox<String> positionComboBox;
     @FXML
     private TextField ageTxt;
     @FXML
@@ -37,6 +40,7 @@ public class TeacherRegisterController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         genderComboBox.setItems(FXCollections.observableArrayList("Male", "Female", "Other"));
+        positionComboBox.setItems(FXCollections.observableArrayList("Junior", "Senior", "Parttime"));
     }
 
     @FXML
@@ -46,17 +50,18 @@ public class TeacherRegisterController implements Initializable {
     }
 
     @FXML
-    public void register(ActionEvent e) {
+    /*public void register(ActionEvent e) {
         String username = usernameTxt.getText();
         String name = nameTxt.getText();
         String gender = genderComboBox.getValue();
+        String position = positionComboBox.getValue();
         String age = ageTxt.getText();
         String department = departmentTxt.getText();
         String password = passwordTxt.getText();
         String confirmPassword = confirmPasswordTxt.getText();
 
         try {
-            if (username.isEmpty() || name.isEmpty() || gender == null || age.isEmpty() || Integer.parseInt(age) < 0 || department.isEmpty() || password.isEmpty() || !password.equals(confirmPassword)) {
+            if (username.isEmpty() || name.isEmpty() || gender == null || position==null || age.isEmpty() || Integer.parseInt(age) < 0 || department.isEmpty() || password.isEmpty() || !password.equals(confirmPassword)) {
                 errorMessageLbl.setText("Error: Please check your inputs.");
                 errorMessageLbl.setVisible(true);
                 return;
@@ -68,7 +73,7 @@ public class TeacherRegisterController implements Initializable {
         }
 
 
-        String sql = "INSERT INTO teachers (username, name, gender, age, department, password) VALUES (?, ?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO teacher (username, name, gender, age, position, department, password) VALUES (?, ?, ?, ?, ?, ?, ?)";
 
         try (Connection conn = DatabaseConnection.getConnection();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
@@ -77,8 +82,9 @@ public class TeacherRegisterController implements Initializable {
             pstmt.setString(2, name);
             pstmt.setString(3, gender);
             pstmt.setInt(4, Integer.parseInt(age));
-            pstmt.setString(5, department);
-            pstmt.setString(6, password);
+            pstmt.setString(5,position);
+            pstmt.setString(6, department);
+            pstmt.setString(7, password);
             int rowsAffected = pstmt.executeUpdate();
 
             if (rowsAffected > 0) {
@@ -100,6 +106,84 @@ public class TeacherRegisterController implements Initializable {
             errorMessageLbl.setText("Error connecting to the database.");
             errorMessageLbl.setVisible(true);
         }
+    }*/
+
+    private void register() {
+        String username = usernameTxt.getText();
+        String name = nameTxt.getText();
+        String gender = genderComboBox.getValue();
+        String position = positionComboBox.getValue();
+        String age = ageTxt.getText();
+        String department = departmentTxt.getText();
+        String password = passwordTxt.getText();
+        String confirmPassword = confirmPasswordTxt.getText();
+
+        try {
+            if (username.isEmpty() || name.isEmpty() || gender == null || age.isEmpty() || Integer.parseInt(age) < 0 || position == null || department.isEmpty() || password.isEmpty()|| !password.equals(confirmPassword)) {
+                errorMessageLbl.setText("Error: Please check your inputs.");
+                errorMessageLbl.setStyle("-fx-text-fill: red;");
+                errorMessageLbl.setVisible(true);
+                return;
+            }
+        } catch (NumberFormatException exception) {
+            errorMessageLbl.setText("Error: Please check your inputs.");
+            errorMessageLbl.setStyle("-fx-text-fill: red;");
+            errorMessageLbl.setVisible(true);
+            return;
+        }
+
+        String checkSql = "SELECT COUNT(*) FROM teacher WHERE username = ?";
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement checkStmt = conn.prepareStatement(checkSql)) {
+
+            checkStmt.setString(1, username);
+            ResultSet rs = checkStmt.executeQuery();
+            if (rs.next() && rs.getInt(1) > 0) {
+                errorMessageLbl.setText("Error: Username already exists.");
+                errorMessageLbl.setStyle("-fx-text-fill: red;");
+                errorMessageLbl.setVisible(true);
+                return;
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            errorMessageLbl.setText("Error connecting to the database.");
+            errorMessageLbl.setStyle("-fx-text-fill: red;");
+            errorMessageLbl.setVisible(true);
+            return;
+        }
+
+        String sql = "INSERT INTO teacher (username, name, gender, age, position, department, password) VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, username);
+            pstmt.setString(2, name);
+            pstmt.setString(3, gender);
+            pstmt.setInt(4, Integer.parseInt(age));
+            pstmt.setString(5, position);
+            pstmt.setString(6, department);
+            pstmt.setString(7, password);
+            int rowsAffected = pstmt.executeUpdate();
+
+            if (rowsAffected > 0) {
+                errorMessageLbl.setText("Add Successful!");
+                errorMessageLbl.setStyle("-fx-text-fill: green;");
+                errorMessageLbl.setVisible(true);
+            } else {
+                errorMessageLbl.setText("Failed to add teacher.");
+                errorMessageLbl.setStyle("-fx-text-fill: red;");
+                errorMessageLbl.setVisible(true);
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            errorMessageLbl.setText("Error connecting to the database.");
+            errorMessageLbl.setStyle("-fx-text-fill: red;");
+            errorMessageLbl.setVisible(true);
+        }
     }
+
 }
 
